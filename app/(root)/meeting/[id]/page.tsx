c0073f321c6634f00c2840acc80e65e0bce04e6a
s1ddiq <siddiq.nasheed@gmail.com>
2025-01-19 17:50:19 -0500

    params fix test hehe

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -9,30 +9,31 @@ import { useUser } from '@clerk/nextjs'
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
 import React, { useState } from 'react'
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+// Typing the component props properly
+interface MeetingProps {
+  params: { id: string };
+}
 
-  const {isLoaded} = useUser();
+const Meeting: React.FC<MeetingProps> = ({ params }) => {
+  const { isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(params.id);
 
-
-  const [isSetupComplete, setIsSetupComplete] = useState(false)
-
+  if (!isLoaded || isCallLoading) return <Loader />;
   
-  const {call, isCallLoading} = useGetCallById(params.id);
-
-  if(!isLoaded || isCallLoading) return <Loader/>
   return (
     <main className="h-screen w-full">
       <StreamCall call={call}>
         <StreamTheme>
           {!isSetupComplete ? (
-            <MeetingSetup setIsSetupComplete={setIsSetupComplete}/>
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
           ) : (
             <MeetingRoom />
           )}
         </StreamTheme>
       </StreamCall>
     </main>
-  )
-}
+  );
+};
 
-export default Meeting
+export default Meeting;
