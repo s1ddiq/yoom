3870943687e198ba49492c3215f26b381f1c4068
s1ddiq <siddiq.nasheed@gmail.com>
2025-01-23 14:32:14 -0500

    test commits

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,24 +1,31 @@
 "use client";
 
-import Loader from '@/components/Loader';
-import MeetingRoom from '@/components/ui/MeetingRoom';
-import MeetingSetup from '@/components/ui/MeetingSetup';
-import { useGetCallById } from '@/hooks/useGetCallById';
-import { useUser } from '@clerk/nextjs';
-import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
-import { useState } from 'react';
+import Loader from "@/components/Loader";
+import MeetingTypeList from "@/components/MeetingTypeList";
+import MeetingRoom from "@/components/ui/MeetingRoom";
+import MeetingSetup from "@/components/ui/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useState } from "react";
 
-export default function Meeting({ params }: { params: { id: string } }) {
+import { FC } from "react";
+
+interface MeetingPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const Meeting: FC<MeetingPageProps> = ({ params }) => {
   const { isLoaded } = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false);
 
-  // Use the custom hook to fetch call details
   const { call, isCallLoading } = useGetCallById(params.id);
 
   if (!isLoaded || isCallLoading) {
     return <Loader />;
   }
-
   return (
     <main className="h-screen w-full">
       <StreamCall call={call}>
@@ -32,4 +39,6 @@ export default function Meeting({ params }: { params: { id: string } }) {
       </StreamCall>
     </main>
   );
-}
+};
+
+export default Meeting;
